3d8ffe0965d405660b48d69c9925ebb521e470be
Sanjai kumar <119435129+sanjai-AK47@users.noreply.github.com>
2023-12-09 13:15:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='subprober',
-    version='1.0.2',
+    version='1.0.3',
     author='D. Sanjai Kumar',
     author_email='bughunterz0047@gmail.com',
-    description='Subrpober - A http protocol probing tool',
+    description='Subprober - A Fast Probing Tool for Penetration testing',
     packages=find_packages(),
     install_requires=[
-        'colorama==0.4.4',
-        'httpx==0.25.0',
-        'requests==2.31.0',
-        'argparse==1.4.0',
-        'beautifulsoup4==4.11.1',
+        'colorama>=0.4.4',
+        'httpx>=0.25.0',
+        'requests>=2.31.0',
+        'argparse>=1.4.0',
+        'beautifulsoup4>=4.11.1',
+        'alive_progress>=3.1.4',
     ],
     entry_points={
         'console_scripts': [
